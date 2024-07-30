15d8a8ee5d02ad967c0a08d6ca278c3d23739931
Doaa alhaji <fgdoaa5@gmail.com>
2024-07-30 23:49:42 +0300

    doaacommit to ensure the upload process

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/doaacommit', function () {
+    return "This is the first commitment to ensure the upload process";
+});
